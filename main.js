12b760c80fc7a416e7754b307e5f9e0951fab041
unknown <tmcfall.prsvr@gmail.com>
2023-12-17 23:14:24 -0500

    final

--- main.js
+++ main.js
@@ -19,12 +19,27 @@ if(str.length >= 15){
 }
 }
 
-function exclaim(str) {
-
+function exclaim(sentence) {
+  if(sentence[sentence.length-1]!== "!"){
+    return sentence + "!"
+  } else {
+    let str = ""
+    for(char of sentence){
+      if(char !== "!"){
+        str = str + char
+      } else {
+        str = str
+      }
+    }
+    return str + "!"
+  }
 }
 
-function countWords() {
-  
+
+
+
+function countWords(str) {
+  return str.split(" ").length
 }
 
 function containsDigit(str) {
@@ -32,37 +47,78 @@ function containsDigit(str) {
 }
 
 
-function containsLowerCase() {
-
+function containsLowerCase(str) {
+  return (/[a-z]/.test(str))
 }
 
-function containsUpperCase() {
-
+function containsUpperCase(str) {
+  return (/[A-Z]/.test(str))
 }
 
-function containsNonAlphanumeric() {
+function containsNonAlphanumeric(str) {
+  let count = 0
+  for(let char of str){
+    if(containsLowerCase(char)=== true){
+      count = count + 1
+    } else if (containsUpperCase(char)===true){
+      count = count + 1
+    } else if (containsDigit(char)===true){
+      count = count + 1
+    }
+  }
+  return str.length !== count
+  }
 
+
+function containsSpace(str) {
+  return str.indexOf(' ') !== -1
 }
 
-function containsSpace() {
-
+function digits(num) {
+  let array = num.toString().split("")
+  let final = []
+  for(let char of array){
+    if(containsNonAlphanumeric(char)=== true){
+    } else {
+      char = parseFloat(char)
+      final.push(char)
+    }
+  } return final
 }
 
-function digits() {
-  
-}
-
-function truncate() {
-
+function truncate(str) {
+  if(isLong(str) === false){
+    return str
+  }else {
+    final = ""
+    for(let char of str){
+      if(final.length <= 7){
+        final = final + char
+      }
+    }
+  }
+ return final + "..."
 }
 
 function isValidPassword() {
   
 }
 
-function onlyPunchy() {
+function onlyPunchy(arr) {
+  let punchy = []
+  for(let str of arr){
+    str = exclaim(str)
+    if(isLong(str) === false){
+      punchy.push(str)
+  
+  }
+    }
+    return punchy
+  }
+  
 
-}
+  
+
 
 
 module.exports = {
